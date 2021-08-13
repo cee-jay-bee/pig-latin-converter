0b1bb89248bd00d2196adf83c39c7204775c32d2
CJ Barnes <cristopher.barnes@gmail.com>
2021-08-13 10:03:33 -0400

    Updated to account for vowels, second consonants, and capitalize first word

--- client.js
+++ client.js
@@ -18,9 +18,10 @@ const turnStringSentenceIntoArray = sentence => {
     for (let i = 0; i < arrayOfSpaceIndices.length; i++){
         arrayOfWords.push(sentence.slice(arrayOfSpaceIndices[i] + 1, arrayOfSpaceIndices[i+1])); //slice from 1st space to 2nd space (then 2nd to 3rd, then 3rd to 4th... finally last space to end)
     } 
-
+   
     //return the resulting array of words
     return arrayOfWords;
+    
 }
 
 //{thing 2a} : function that takes in an array and runs each word through the pigLatinWordConverter function (thing 2b)
@@ -37,6 +38,7 @@ const piglatinifyWholeSentence = array => {
     pigLatinString = pigLatinArray.join(" ");   
     
     //todo - add logic here to capitalize first letter of the sentence?
+    pigLatinString = pigLatinString[0].toUpperCase() + pigLatinString.slice(1);
 
     //return the resulting string
     return pigLatinString;
@@ -46,32 +48,51 @@ const piglatinifyWholeSentence = array => {
 const pigLatinWordConverter = inputWord => {
     //make all letters lowercase
     inputWord = inputWord.toLowerCase();
+    let vowels = `aeiou`;
 
-    //take first letter out and store in varible
-    let firstLetter = inputWord.slice(0,1);
+    //logic for adding 'way to word if it starts with vowel
+    for (let i = 0; i < vowels.length; i++){
+        //check to see if the first letter matches any vowel
+        if (inputWord[0] === vowels[i]){
+            //add 'way' to the inputWord if there is a match
+            return inputWord.concat('way');
+        }
+
+        if (inputWord[1] === vowels[i]){
+            //if firstLetter is not any vowel, but the second letter is, take first letter out and store in variable
+            let firstLetter = inputWord.slice(0,1);
+            //slice out rest of word and store in variable
+            let restOfWord = inputWord.slice(1);
+            //add firstLetter and 'ay' to end of restOfWord and return word
+            return restOfWord.concat(firstLetter, `ay`);
+        }
+    }
+    //if first two letters are not vowels, take first two letters out and store in variable    
+    let firstTwoLetters = inputWord.slice(0,2);
 
     //slice out rest of word and store in variable
-    let restOfWord = inputWord.slice(1);
+    let wordLessFirstTwo = inputWord.slice(2);
 
-    //add firstLetter and 'ay' to end of restOfWord
-    let pigLatin = restOfWord.concat(firstLetter, `ay`);
+    //add first two letters to end of the rest of the word and 'ay' to the end
+    return wordLessFirstTwo.concat(firstTwoLetters, 'ay');
 
-    //todo - this does not include special case where word begins with vowel - 
-    //according to a pig latin dictionary we found, then we add "way" instead of "ay"
-    //e.g. and -> andway in piglatin
-    //so, the todo would be to add logic for that here. 
-
-    //return the resulting "piglatin-afied word"
-    return pigLatin;
 }
 
 
 //{Finally, use all the above code} 
 
-//start by inputting a sentence
-let sentence = `Dunkin Donuts has coffee and some mediocre donuts`; 
 //todo change this to a prompt so user can input their own sentence
-//todo - also make sure punctuatuon is not allowed (if contatins punctuation, ask user to try again?)
+const getSentence = () => {
+    if (confirm('Ready to learn Pig Latin?')){
+        return prompt(`What sentence do you want to translate? (no punctuation)`);
+    }
+}
+
+
+//start by inputting a sentence 
+let sentence = getSentence();
+
+//todo - also make sure punctuatuon is not allowed (if contains punctuation, ask user to try again?)
 
 //turn that into an array of words
 let arrayfiedSentence = turnStringSentenceIntoArray(sentence);
